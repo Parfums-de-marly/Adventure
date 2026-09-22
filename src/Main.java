4b5175c66cd63fac630b0be9067122ec676ce79b
Alphahyde54 <daniel@oxboell.dk>
2026-09-22 15:54:03 +0200

    Deleted UI and created Room class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,2 @@
 void main() {
-    IO.println("Lucas er til mænd");
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,43 @@
 public class Room {
+String name;
+String description;
+Room north;
+Room east;
+Room south;
+Room west;
+
+    public Room(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+    public Room setNorth(Room room){
+        return north = room;
+    }
+    public Room getNorth(){
+        return north;
+    }
+
+    public Room setEast(Room room){
+        return east = room;
+    }
+
+    public Room getEast(){
+        return east;
+    }
+
+    public Room setSouth(Room room){
+        return south = room;
+    }
+
+    public Room getSouth(){
+        return south;
+    }
+
+    public Room setWest(Room room){
+        return west = room;
+    }
+
+    public Room getWest(){
+        return west;
+    }
 }
