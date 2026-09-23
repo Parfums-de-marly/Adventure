0acf7b7bc7ed2c2096173c8ccf6abace49460ca3
Lucas <lucasskov00@gmail.com>
2026-09-23 09:50:44 +0200

    Layout and movement done

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,17 +1,48 @@
 public class Adventure {
-    Room room1;
-    Room room2;
-    Room room3;
-    Room room4;
-    Room room5;
-    Room room6;
-    Room room7;
-    Room room8;
-    Room room9;
+    Room room1 = new Room("Room 1", "Et lille stenkammer med kolde vægge og en flimrende fakkel.");
+    Room room2 = new Room("Room 2", "Et snævert rum fyldt med spindelvæv og støvede kister.");
+    Room room3 = new Room("Room 3", "En lille lysning med en gammel offersten i midten.");
+    Room room4 = new Room("Room 4", "Et trangt bibliotek med væltede boghylder og papirer på gulvet.");
+    Room room5 = new Room("Room 5", "Et koldt kammer dækket af rim, med et frosset vandfald i hjørnet.");
+    Room room6 = new Room("Room 6", "En lille smedje med en rustet ambolt og kolde ovne.");
+    Room room7 = new Room("Room 7", "Et mørkt kapel med knækkede statuer og lugten af gammelt voks.");
+    Room room8 = new Room("Room 8", "Et lille lager fyldt med tomme tønder og en skæv lampe.");
+    Room room9 = new Room("Room 9", "Et snævert vagtrum med en rusten rustning lænet op ad væggen.");
 
-    Room currentRoom;
+
+    Room currentRoom = room1;
     boolean adventureIsDone = false;
 
+
+    public Adventure(){
+
+        room1.setEast(room2);
+        room2.setWest(room1);
+
+        room2.setEast(room3);
+        room3.setWest(room2);
+
+        room4.setNorth(room1);
+        room1.setSouth(room4);
+
+        room3.setSouth(room6);
+        room6.setNorth(room3);
+
+        room6.setSouth(room9);
+        room9.setNorth(room6);
+
+        room9.setWest(room8);
+        room8.setEast(room9);
+
+        room8.setNorth(room5);
+        room5.setSouth(room8);
+
+        room8.setWest(room7);
+        room7.setEast(room8);
+
+        room7.setNorth(room4);
+        room4.setSouth(room7);
+    }
 
     public void run() {
 
@@ -20,36 +51,38 @@ public class Adventure {
             String commandInput = IO.readln("Input Direction(north, south, east, west): ");
             switch (commandInput) {
                 case "north" -> {
-                    IO.println("You go: North");
+                    if (currentRoom.getNorth() != null){
+                        currentRoom = currentRoom.getNorth();
+                        IO.println("You go: North");
+
+                    } else {
+                    IO.println("You cant go North");}
                 }
                 case "south" -> {
-                    IO.println("You go: South");
+                    if (currentRoom.getSouth() != null){
+                        currentRoom = currentRoom.getSouth();
+                        IO.println("You go: South");
+                    } else{
+                    IO.println("You cant go: South");}
                 }
                 case "east" -> {
-                    IO.println("You go: East");
+                    if (currentRoom.getEast() != null){
+                        currentRoom = currentRoom.getEast();
+                        IO.println("You go East");
+                    } else {
+                    IO.println("You cant go East");}
                 }
                 case "west" -> {
-                    IO.println("You go: West");
-                }
+                    if (currentRoom.getWest() != null){
+                        currentRoom = currentRoom.getWest();
+                        IO.println("You go West");
+                    } else {
+                    IO.println("You cant go West");
+                }}
             }
         }
     }
 
-
-    public Adventure(){
-        room1 = new Room("Room 1", "Et lille stenkammer med kolde vægge og en flimrende fakkel.");
-        room2 = new Room("Room 2", "Et snævert rum fyldt med spindelvæv og støvede kister.");
-        room3 = new Room("Room 3", "En lille lysning med en gammel offersten i midten.");
-        room4 = new Room("Room 4", "Et trangt bibliotek med væltede boghylder og papirer på gulvet.");
-        room5 = new Room("Room 5", "Et koldt kammer dækket af rim, med et frosset vandfald i hjørnet.");
-        room6 = new Room("Room 6", "En lille smedje med en rustet ambolt og kolde ovne.");
-        room7 = new Room("Room 7", "Et mørkt kapel med knækkede statuer og lugten af gammelt voks.");
-        room8 = new Room("Room 8", "Et lille lager fyldt med tomme tønder og en skæv lampe.");
-        room9 = new Room("Room 9", "Et snævert vagtrum med en rusten rustning lænet op ad væggen.");
-
-        // Forbind rooms her
-        // room1.setEast(room2); sådan her måske?
-    }
 
 
 
